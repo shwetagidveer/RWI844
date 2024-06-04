a92433816403211c376025f71ae6fefef7217576
shwetagidveer <155894507+shwetagidveer@users.noreply.github.com>
2024-06-04 13:28:32 +0530

    Update CompositionExample.java

--- CompositionExample.java
+++ CompositionExample.java
@@ -10,7 +10,8 @@ class Office {
         this.name = name;     
         this.address = address;     
     }     
-}     
+}  
+   
 // Create a class for Company that has more than one Office.     
 class Company {     
     // declare variable for company name  
@@ -35,25 +36,25 @@ class CompositionExample {
         public static void main(String[] args)     
         {     
                 // create objects of Office class.     
-            Office o1 = new Office("Google India Pvt. Ltd", "Banglore");     
-            Office o2 = new Office("Google India Pvt. Ltd", "Gurgaon");  
-                Office o3 = new Office("Google India Pvt. Ltd", "Hyderabad");  
-                Office o4 = new Office("Google India Pvt. Ltd", "Mumbai");  
+            Office o1 = new Office("RailWorld India Pvt. Ltd", "Pune");     
+            Office o2 = new Office("RailWorld India Pvt. Ltd", "Gurgaon");  
+                Office o3 = new Office("RailWorld India Pvt. Ltd", "Hyderabad");  
+                Office o4 = new Office("RailWorld India Pvt. Ltd", "Indore");  
                 // create list which contains the no. of offices     
                 List<Office> offices = new ArrayList<Office>();     
                 offices.add(o1);     
                 offices.add(o2);     
                 offices.add(o3);   
                 offices.add(o4);   
-             // create an instance of the Company class by passing a list of Office to the constructor  
-                Company company = new Company("Google", offices);     
+             // create an instance of the Company class by passing a list of Office to the                         constructor  
+                Company company = new Company("RailWorld", offices);     
                 System.out.println(company.name+" offices:");  
              // get total offices of the Company using getter  
                 List<Office> data = company.getTotalOfficesOfCompany();  
                 // iterate offices of the company   
                 for (Office office : data) {     
                     // print data   
-                        System.out.println("Name : " + office.name + " and "+ " Address : " + office.address);    
+                        System.out.println("Name : " + office.name + " and "+ " Address : " +                                                             office.address);    
                 }     
         }     
 }  
