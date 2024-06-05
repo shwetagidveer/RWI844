eb6a5559056361a5954bf6d83ec1a6ca2ee4b898
shwetagidveer <155894507+shwetagidveer@users.noreply.github.com>
2024-06-05 17:58:08 +0530

    Update PalindromeNoEx.java

--- PalindromeNoEx.java
+++ PalindromeNoEx.java
@@ -1,3 +1,30 @@
+import java.util.Scanner;
+
+public class PalindromeNoEx {
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in); // Create a Scanner object to read input
+        System.out.println("Enter a number to check if it is a palindrome: ");
+        
+        int n = in.nextInt();	 // Read the number from the user
+        int temp = n; 		// Store the original number for comparison
+        int r, sum = 0;
+        
+        while (n > 0) {
+            r = n % 10;	 // Get the remainder (last digit)
+            sum = (sum * 10) + r; // Build the reversed number
+            n = n / 10; 	// Remove the last digit from the number
+        }
+        
+        // Check if the original number is equal to the reversed number
+        if (temp == sum) {
+            System.out.println("Palindrome Number");
+        } else {
+            System.out.println("Not Palindrome");
+        }
+        
+        // No need to close the Scanner in this simple program
+    }
+}
 class PalindromeNoEx{  
  public static void main(String args[]){  
   int r,sum=0,temp;    
