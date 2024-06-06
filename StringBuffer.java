e7801139a200edf1c818cab6e25b7dba591d3f7e
shwetagidveer <155894507+shwetagidveer@users.noreply.github.com>
2024-06-06 15:46:40 +0530

    Update StringBufferEx.java

--- StringBuffer.java
+++ StringBuffer.java
@@ -1,8 +1,10 @@
-public class StringBuffer{  
-    public static void main(String[] args)
-    {  
-        StringBuffer buffer=new StringBuffer("Shweta");  
-        buffer.append("Gidveer");  
-        System.out.println(buffer);  
-    }  
+public class StringBufferEx {
+    public static void main(String[] args) {
+
+        // Explicitly using the built-in StringBuffer class
+        java.lang.StringBuffer buffer = new java.lang.StringBuffer("Shweta");
+        buffer.append(" Gidveer");
+        System.out.println(buffer);   // Output: Shweta Gidveer
+    }
 }
+
